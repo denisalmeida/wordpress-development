843105635b028c3ebe03fe5479a2d5d62c9e9250
Denis Almeida <denisalmeida@me.com>
2024-10-19 23:55:28 -0300

    Revert ":rocket: launch tailmint theme"
    This reverts commit 8f93c0031d7c4aa15b50f3035bc0193e21e30683.

--- wp-content/themes/tailmint/footer.php
+++
@@ -1,8 +0,0 @@
-	</main><!-- #main -->
-	<footer class="site-footer py-8 text-center text-dark border-t border-gray-200">
-		© <?php echo date('Y'); ?> Denis Almeida.
-	</footer>
-</div><!-- #page -->
-<?php wp_footer(); ?>
-</body>
-</html>
